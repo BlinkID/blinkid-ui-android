ec6028b73407c4bbcd50bda2641a8c4e56868d79
skuzmic <sandra.kuzmic@microblink.com>
2018-10-17 14:15:53 +0200

    add support for back side for EUDl, Italy, Ireland, Colombia and Austria, change default generic document for dl

--- blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/ColombiaDlRecognition.kt
+++ blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/ColombiaDlRecognition.kt
@@ -1,15 +1,21 @@
 package com.microblink.documentscanflow.recognition.implementations
 
 import com.microblink.documentscanflow.R
+import com.microblink.documentscanflow.buildDetectorRecognizerFromPreset
 import com.microblink.documentscanflow.isEmpty
 import com.microblink.documentscanflow.recognition.BaseRecognition
+import com.microblink.entities.detectors.quad.document.DocumentSpecificationPreset
+import com.microblink.entities.recognizers.Recognizer
 import com.microblink.entities.recognizers.blinkid.colombia.ColombiaDlFrontRecognizer
 
 class ColombiaDlRecognition : BaseRecognition() {
 
     private val frontRecognizer by lazy { ColombiaDlFrontRecognizer() }
+    private val backRecognizer by lazy {
+        buildDetectorRecognizerFromPreset(DocumentSpecificationPreset.DOCUMENT_SPECIFICATION_PRESET_ID1_CARD)
+    }
 
-    override fun getSingleSideRecognizers() = listOf(frontRecognizer)
+    override fun getSingleSideRecognizers() = listOf<Recognizer<*, *>>(frontRecognizer, backRecognizer)
 
     override fun extractData(): String? {
         val result = frontRecognizer.result
--- blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/ItalyDlRecognition.kt
+++ blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/ItalyDlRecognition.kt
@@ -1,16 +1,22 @@
 package com.microblink.documentscanflow.recognition.implementations
 
 import com.microblink.documentscanflow.R
+import com.microblink.documentscanflow.buildDetectorRecognizerFromPreset
 import com.microblink.documentscanflow.isEmpty
 import com.microblink.documentscanflow.recognition.BaseRecognition
 import com.microblink.documentscanflow.recognition.util.FormattingUtils
+import com.microblink.entities.detectors.quad.document.DocumentSpecificationPreset
+import com.microblink.entities.recognizers.Recognizer
 import com.microblink.entities.recognizers.blinkid.italy.ItalyDlFrontRecognizer
 
 class ItalyDlRecognition : BaseRecognition() {
 
     private val frontRecognizer by lazy { ItalyDlFrontRecognizer() }
+    private val backRecognizer by lazy {
+        buildDetectorRecognizerFromPreset(DocumentSpecificationPreset.DOCUMENT_SPECIFICATION_PRESET_ID1_CARD)
+    }
 
-    override fun getSingleSideRecognizers() = listOf(frontRecognizer)
+    override fun getSingleSideRecognizers() = listOf<Recognizer<*, *>>(frontRecognizer, backRecognizer)
 
     override fun extractData(): String? {
         val result = frontRecognizer.result
