d4dad7797429e9a1d83852434f73723689af8663
Matej Vidaković <matej.vidakovic@microblink.com>
2018-12-21 09:01:30 +0100

    fix for Switzerland id recognition using Croatia string combiner

--- blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/SwitzerlandIdRecognition.kt
+++ blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/SwitzerlandIdRecognition.kt
@@ -54,7 +54,7 @@ class SwitzerlandIdRecognition: BaseTwoSideRecognition() {
         var lastName: String? = ""
         when {
             isCombinedScan(frontResult, backResult) -> {
-                val stringCombiner = StringCombiner(StringCombiner.Country.CROATIA)
+                val stringCombiner = StringCombiner(StringCombiner.Country.SWITZERLAND)
                 firstName = stringCombiner.combineMRZString(backResult.mrzResult.secondaryId, frontResult.givenName)
                 lastName = stringCombiner.combineMRZString(backResult.mrzResult.primaryId, frontResult.surname)
             }
