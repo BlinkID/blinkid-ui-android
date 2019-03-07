b7787ed05fdc6c6ba4a5ce50fe608eec8e8cf088
Matej Vidaković <matej.vidakovic@microblink.com>
2019-03-07 12:00:09 +0100

    supporting es, de and fr now

--- blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/ui/localization/DefaultLocalizationManager.kt
+++ blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/ui/localization/DefaultLocalizationManager.kt
@@ -2,7 +2,7 @@ package com.microblink.documentscanflow.ui.localization
 
 internal class DefaultLocalizationManager: LocalizationManager {
 
-    private val supportedLanguages = setOf("en")
+    private val supportedLanguages = setOf("en", "es", "de", "fr")
 
     override fun isLanguageSupported(lang: String) = supportedLanguages.contains(lang)
 
