cd4b07a68e392381aae69b0ec702d5a8ddb64ec1
Matej Vidaković <matej.vidakovic@microblink.com>
2019-03-28 14:08:38 +0100

    no longer returning deprecated usdl fields

--- blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/NigeriaDlRecognition.kt
+++ blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/NigeriaDlRecognition.kt
@@ -1,10 +1,3 @@
 package com.microblink.documentscanflow.recognition.implementations
 
-import com.microblink.entities.recognizers.blinkbarcode.usdl.UsdlKeys
-
-class NigeriaDlRecognition: UsdlRecognition() {
-
-    override val keysToExcludeFromResults: Set<UsdlKeys>
-        get() = setOf(UsdlKeys.Height, UsdlKeys.HeightCm, UsdlKeys.HeightIn)
-
-}
+class NigeriaDlRecognition: UsdlRecognition()
--- blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/UsdlRecognition.kt
+++ blinkid-ui-lib/src/main/kotlin/com/microblink/documentscanflow/recognition/implementations/UsdlRecognition.kt
@@ -1,23 +1,16 @@
 package com.microblink.documentscanflow.recognition.implementations
 
-import android.text.TextUtils
 import com.microblink.documentscanflow.isEmpty
-import com.microblink.documentscanflow.recognition.resultentry.StringResultEntry
 import com.microblink.documentscanflow.recognition.BaseRecognition
-import com.microblink.documentscanflow.recognition.resultentry.ResultKey
-import com.microblink.documentscanflow.recognition.util.FormattingUtils
 import com.microblink.entities.recognizers.Recognizer
-import com.microblink.entities.recognizers.blinkbarcode.usdl.UsdlKeys
 import com.microblink.entities.recognizers.blinkbarcode.usdl.UsdlRecognizer
 import com.microblink.entities.recognizers.blinkid.documentface.DocumentFaceRecognizer
+import com.microblink.documentscanflow.recognition.resultentry.ResultKey.*
 
 open class UsdlRecognition : BaseRecognition() {
 
     private val recognizer by lazy { UsdlRecognizer() }
     private val faceRecognizer by lazy { DocumentFaceRecognizer() }
-
-    protected open val keysToExcludeFromResults
-        get() = setOf<UsdlKeys>()
 
     override fun getSingleSideRecognizers(): List<Recognizer<*, *>> {
         return listOf(faceRecognizer, recognizer)
@@ -29,24 +22,21 @@ open class UsdlRecognition : BaseRecognition() {
             return null
         }
 
-        val sb = StringBuilder()
-        sb.append(result.getField(UsdlKeys.CustomerFirstName))
-        val middleName = result.getField(UsdlKeys.CustomerMiddleName)
-        if (!middleName.isEmpty()) {
-            sb.append(" ").append(middleName)
-        }
-        val fullName = FormattingUtils.formatResultTitle(
-                sb.toString(),
-                result.getField(UsdlKeys.CustomerFamilyName))
+        add(FIRST_NAME, result.firstName)
+        add(LAST_NAME, result.lastName)
+        add(FULL_NAME, result.fullName)
+        add(DATE_OF_BIRTH, result.dateOfBirth)
+        add(DATE_OF_ISSUE, result.dateOfIssue)
+        addDateOfExpiry(result.dateOfExpiry.date)
+        add(DOCUMENT_NUMBER, result.documentNumber)
+        add(ADDRESS, result.address)
+        add(SEX, result.sex)
+        add(VEHICLE_CLASS, result.vehicleClass)
+        add(ENDORSEMENTS, result.endorsements)
+        add(DRIVER_RESTRICTIONS, result.restrictions)
+        add(BARCODE_DATA, result.rawStringData)
 
-        for (key in UsdlKeys.values()) {
-            val value = result.getField(key)
-            if (!TextUtils.isEmpty(value) && !keysToExcludeFromResults.contains(key)) {
-                resultEntries.add(StringResultEntry(ResultKey.fromUsdlKey(key), value))
-            }
-        }
-
-        return fullName
+        return result.fullName
     }
 
 }
